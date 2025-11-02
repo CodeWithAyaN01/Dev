457ad544f5c3291f8f759688b4398c15046e0212
CodeWithAyaN01 <ayankrrish@gmail.com>
2025-11-02 19:25:21 +0530

    dd

---
+++ learn1/src/App1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <>
+      <div id ="div1">
+        <h1>this is div 1</h1>
+      </div>
+      <div id = "div2">
+          <h1>this is div 2</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
