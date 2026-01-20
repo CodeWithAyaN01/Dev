9c5924b43d4d9d30313ad3f4ef349695fbb56c37
CodeWithAyaN01 <ayankrrish@gmail.com>
2026-01-20 22:04:19 +0530

    All about buffers

---
+++ Backend_Node/04-Buffers/bufferBasics.js
@@ -0,0 +1,25 @@
+const { Buffer } = require('buffer')
+
+
+// const buf = Buffer.from("Hello bhai kaise ho")
+// console.log(buf)
+// console.log(buf.toString())
+
+
+// const buf = Buffer.alloc(10) // 10 bytes allocated
+// buf.write("kaise ho") // writes
+// console.log(buf) // buffer
+// console.log(buf.toString())
+
+
+// const buf = Buffer.from("hello buffer")
+// console.log(buf.toString())
+// console.log(buf.toString('utf8',0,5)) // index 0 to 4 only
+
+
+// const buf1 = Buffer.from("hello bro")
+// const buf2 = Buffer.from(" code karo")
+// const merged = Buffer.concat([buf1,buf2])
+// console.log(merged.toString())
+// console.log(merged.length)
+
